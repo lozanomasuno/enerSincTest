0235ebefbb7a9c4f1e633ee5f1f9c65a368b39f4
lozanomasuno <39577534+lozanomasuno@users.noreply.github.com>
2022-09-13 09:33:30 -0500

    dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Grid } from '@material-ui/core';
 
 export const Dashboard = () => {
   return (
     <>
-      <div className='container'>
-          <label> DashBoard </label>
+      <div>
+          <Grid container spacing={3}>
+              <Grid item  xs={12}>
+
+              </Grid>
+              <Grid item  xs={12} sm={4} md={4} lg={4} xl={4}>
+                <div className='dashboard-card pink'>
+                    <p> 10 </p>   
+                    <label>Total User loaded</label>   
+                </div>
+              </Grid>
+              <Grid  item  xs={12} sm={4} md={4} lg={4} xl={4}>
+                <div className='dashboard-card red'>
+                    <p> 20 </p>   
+                    <label>Total pending for approved</label>   
+                </div>
+              </Grid>
+              <Grid  item  xs={12} sm={4} md={4} lg={4} xl={4}>
+                <div className='dashboard-card gray'>
+                    <p>30 </p>   
+                    <label>Total Hired</label>   
+                </div>
+              </Grid>
+          </Grid>          
       </div>
     </>
   )
